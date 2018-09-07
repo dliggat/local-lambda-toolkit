953b5dcd6f73fcc704bc71dd58dd642ddff9fd9d
Andrew Lytle <andlytle@amazon.com>
2018-09-06 21:06:09 -0400

    moved custom_resource.py to utils folder

--- tests/test_custom_resource.py
+++ tests/test_custom_resource.py
@@ -12,7 +12,7 @@ import awslambda
 from .lambda_helpers import MockLambdaContext, MockLambdaEvents
 
 
-@patch('awslambda.custom_resource.CFNCustomResource._send_response')
+@patch('awslambda.utils.custom_resource.CFNCustomResource._send_response')
 class TestCustomResourceHandler(unittest.TestCase):
     def test_handler_create(self, mock_send_response):
         """
@@ -22,7 +22,7 @@ class TestCustomResourceHandler(unittest.TestCase):
         test_context = MockLambdaContext()
         test_event = MockLambdaEvents.cloudformation_creation
 
-        custom_resource = awslambda.custom_resource.CFNCustomResource(test_event, test_context)
+        custom_resource = awslambda.utils.custom_resource.CFNCustomResource(test_event, test_context)
         custom_resource.handler()
         mock_send_response.assert_called_with('SUCCESS', {'Message': 'Resource creation successful!'})
 
@@ -36,7 +36,7 @@ class TestCustomResourceHandler(unittest.TestCase):
         test_context = MockLambdaContext()
         test_event = MockLambdaEvents.cloudformation_update
 
-        custom_resource = awslambda.custom_resource.CFNCustomResource(test_event, test_context)
+        custom_resource = awslambda.utils.custom_resource.CFNCustomResource(test_event, test_context)
         custom_resource.handler()
         mock_send_response.assert_called_with('SUCCESS', {'Message': 'Resource update successful!'})
 
@@ -50,7 +50,7 @@ class TestCustomResourceHandler(unittest.TestCase):
         test_context = MockLambdaContext()
         test_event = MockLambdaEvents.cloudformation_deletion
 
-        custom_resource = awslambda.custom_resource.CFNCustomResource(test_event, test_context)
+        custom_resource = awslambda.utils.custom_resource.CFNCustomResource(test_event, test_context)
         custom_resource.handler()
         mock_send_response.assert_called_with('SUCCESS', {'Message': 'Resource deletion successful!'})
 
