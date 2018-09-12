e61b982c8740dd8b1b1709e759f86e408b4d4d7a
Andrew Lytle <andlytle@amazon.com>
2018-09-11 20:38:16 -0400

    added optional / required parameters

--- awslambda/utils/cfn_macro.py
+++ awslambda/utils/cfn_macro.py
@@ -3,10 +3,22 @@ from awslambda.utils.helpers import Helpers
 
 
 class CFNMacro(object):
+
+    required_params = set()
+    optional_params = set()
+
     RESULT_SUCCESS = "success"
     RESULT_FAILURE = "failure"
 
     def __init__(self, event, context):
+        self.logger = Helpers.setup_logging(context.aws_request_id)
+        if bool(os.getenv('IS_LOCAL')):
+            self.logger.debug('$IS_LOCAL set; likely running in development')
+            self.is_local = True
+        else:
+            self.logger.debug('No $IS_LOCAL set; likely running in Lambda')
+            self.is_local = False
+
         if None in (event, context):
             message = 'Lambda event or context cannot be None'
             self.logger.error(message)
@@ -23,13 +35,13 @@ class CFNMacro(object):
         self.template_parameter_values = self.event['templateParameterValues']
         self.request_id = self.event['requestId']
 
-        self.logger = Helpers.setup_logging(context.aws_request_id)
-        if bool(os.getenv('IS_LOCAL')):
-            self.logger.debug('$IS_LOCAL set; likely running in development')
-            self.is_local = True
-        else:
-            self.logger.debug('No $IS_LOCAL set; likely running in Lambda')
-            self.is_local = False
+        unknown_params = set(self.params.keys()) - (self.required_params.union(self.optional_params))
+        if len(unknown_params):
+            raise KeyError('Unknown parameters: {}'.format(unknown_params))
+
+        missing_params = set(self.required_params - self.params.keys())
+        if len(missing_params):
+            raise KeyError('Missing required parameters: {}'.format(missing_params))
 
         return
 
--- tests/test_cfn_macro.py
+++ tests/test_cfn_macro.py
@@ -15,9 +15,6 @@ from awslambda.utils.cfn_macro import CFNMacro
 
 class TestCFNMacroHandler(unittest.TestCase):
     def test_handler(self):
-        """
-        Test the handler operates as expected.
-        """
         request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
         test_event = {
             "transformId": "123456789012::TestMacro",
@@ -27,7 +24,6 @@ class TestCFNMacroHandler(unittest.TestCase):
             "fragment": {},
             "region": "ca-central-1",
             "params": {
-                "SomeOtherKey": ["SomeOtherValue"]
             },
             "requestId": request_id,
             "accountId": "123456789012"
@@ -36,6 +32,144 @@ class TestCFNMacroHandler(unittest.TestCase):
 
         cfn_macro = CFNMacro(test_event, test_context)
         result = cfn_macro.success({'test_fragment': 'test_fragment_value'})
+        self.assertEquals(result, {'fragment': {'test_fragment': 'test_fragment_value'},
+                                   'requestId': 'fad23d4b-277d-4977-955b-36d2ba887577',
+                                   'status': 'success'})
+
+    def test_required_parameters_missing(self):
+        request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
+        test_event = {
+            "transformId": "123456789012::TestMacro",
+            "templateParameterValues": {
+                "SomeKey": "SomeValue"
+            },
+            "fragment": {},
+            "region": "ca-central-1",
+            "params": {
+            },
+            "requestId": request_id,
+            "accountId": "123456789012"
+        }
+        test_context = MagicMock()
+
+        class MyTestMacro(CFNMacro):
+            required_params = {'Required1', 'Required2'}
+
+        with self.assertRaises(KeyError):
+            MyTestMacro(test_event, test_context)
+
+    def test_required_parameters(self):
+        request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
+        test_event = {
+            "transformId": "123456789012::TestMacro",
+            "templateParameterValues": {
+                "SomeKey": "SomeValue"
+            },
+            "fragment": {},
+            "region": "ca-central-1",
+            "params": {
+                "Required1": "SomeValue",
+                "Required2": "SomeOtherValue"
+            },
+            "requestId": request_id,
+            "accountId": "123456789012"
+        }
+        test_context = MagicMock()
+
+        class MyTestMacro(CFNMacro):
+            required_params = {'Required1', 'Required2'}
+
+        cfn_macro = MyTestMacro(test_event, test_context)
+
+        result = cfn_macro.success({'test_fragment': 'test_fragment_value'})
+
+        self.assertEquals(result, {'fragment': {'test_fragment': 'test_fragment_value'},
+                                   'requestId': 'fad23d4b-277d-4977-955b-36d2ba887577',
+                                   'status': 'success'})
+
+    def test_unknown_parameters(self):
+        request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
+        test_event = {
+            "transformId": "123456789012::TestMacro",
+            "templateParameterValues": {
+                "SomeKey": "SomeValue"
+            },
+            "fragment": {},
+            "region": "ca-central-1",
+            "params": {
+                "Required1": "SomeValue",
+                "Required2": "SomeOtherValue",
+                "UnknownParam": "SomeValue"
+            },
+            "requestId": request_id,
+            "accountId": "123456789012"
+        }
+        test_context = MagicMock()
+
+        class MyTestMacro(CFNMacro):
+            required_params = {'Required1', 'Required2'}
+
+        with self.assertRaises(KeyError):
+            MyTestMacro(test_event, test_context)
+
+    def test_optional_parameters_missing(self):
+        request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
+        test_event = {
+            "transformId": "123456789012::TestMacro",
+            "templateParameterValues": {
+                "SomeKey": "SomeValue"
+            },
+            "fragment": {},
+            "region": "ca-central-1",
+            "params": {
+                "Required1": "SomeValue",
+                "Required2": "SomeOtherValue"
+            },
+            "requestId": request_id,
+            "accountId": "123456789012"
+        }
+        test_context = MagicMock()
+
+        class MyTestMacro(CFNMacro):
+            required_params = {'Required1', 'Required2'}
+            optional_params = {'Optional1'}
+
+        cfn_macro = MyTestMacro(test_event, test_context)
+
+        result = cfn_macro.success({'test_fragment': 'test_fragment_value'})
+
+        self.assertEquals(result, {'fragment': {'test_fragment': 'test_fragment_value'},
+                                   'requestId': 'fad23d4b-277d-4977-955b-36d2ba887577',
+                                   'status': 'success'})
+
+    def test_optional_parameters_included(self):
+        request_id = 'fad23d4b-277d-4977-955b-36d2ba887577'
+        test_event = {
+            "transformId": "123456789012::TestMacro",
+            "templateParameterValues": {
+                "SomeKey": "SomeValue"
+            },
+            "fragment": {},
+            "region": "ca-central-1",
+            "params": {
+                "Required1": "SomeValue",
+                "Required2": "SomeOtherValue",
+                "Optional1": "SomeValue"
+            },
+            "requestId": request_id,
+            "accountId": "123456789012"
+        }
+        test_context = MagicMock()
+
+        class MyTestMacro(CFNMacro):
+            required_params = {'Required1', 'Required2'}
+            optional_params = {'Optional1'}
+
+        cfn_macro = MyTestMacro(test_event, test_context)
+
+        result = cfn_macro.success({'test_fragment': 'test_fragment_value'})
+        self.assertEquals(cfn_macro.optional_params.union(cfn_macro.required_params), test_event.get('params').keys())
+
         self.assertEquals(result, {'fragment': {'test_fragment': 'test_fragment_value'},
                                    'requestId': 'fad23d4b-277d-4977-955b-36d2ba887577',
                                    'status': 'success'})
